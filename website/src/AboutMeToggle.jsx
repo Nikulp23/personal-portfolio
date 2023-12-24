6df189010f1142f60f8a504253e0d1fc3754ac9b
Nikul Patel <pateln14@rpi.edu>
2023-12-25 01:14:38 +0530

    worked on adding logos

--- website/src/AboutMeToggle.jsx
+++ website/src/AboutMeToggle.jsx
@@ -64,9 +64,25 @@ export function AboutSkillsToggle() {
                     </h1>
                     <p className={classes.description}>
                         <Text component="span" variant="gradient" gradient={{ from: 'black', to: 'black' }} inherit>
-                            Nikul Patel
+                        Hello, I'm Nikul Patel, a Computer Science and ITWS student at Rensselaer Polytechnic Institute with a fervent passion for full-stack development. My journey in computer science is driven by a relentless pursuit of knowledge and a desire to turn innovative ideas into tangible solutions.
                         </Text>
                     </p>
+
+                    <br></br>
+
+                    <p className={classes.description}>
+                        <Text component="span" variant="gradient" gradient={{ from: 'black', to: 'black' }} inherit>
+                        I am currently seeking summer internships or co-op opportunities where I can apply my skills in a dynamic and innovative environment. I am enthusiastic about contributing to a team that values creativity, efficiency, and a forward-thinking approach. I am eager to learn from seasoned professionals and to engage in projects that push the boundaries of what's possible in tech.                        </Text>
+                    </p>
+
+                    <br></br>
+
+                    <p className={classes.description}>
+                        <Text component="span" variant="gradient" gradient={{ from: 'black', to: 'black' }} inherit>
+                        Joining your team would be an exciting opportunity to grow and make meaningful contributions. Let's connect and explore the potential for collaboration!
+                        </Text>
+                    </p>
+
                 </motion.div>
               ) : (
                 // Skills Section
